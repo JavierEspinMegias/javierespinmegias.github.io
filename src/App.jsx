a0470b77cb534a3834bfb33e2ceecbf1d0bcf097
Cosmo <javierespinmegias@gmail.com>
2026-02-14 09:01:25 +0100

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Javier Espin Megias",
+  title: "Software Developer & Counter Strike Player",
+  email: "javierespinmegias@gmail.com",
+  gitHub: "https://github.com/JavierEspinMegias",
+  linkedIn: "https://www.linkedin.com/in/javierespin/",
 };
 
 const primaryColor = "#4E567E";
